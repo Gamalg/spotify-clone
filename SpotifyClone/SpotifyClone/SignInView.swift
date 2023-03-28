afe417ad4f01580a6ee4d11c74dff9ab40241cc6
Gamal Kubeyev <kubeyev.gamal@gmail.com>
2023-03-28 22:58:02 +0600

    SPM

---
+++ SpotifyClone/SpotifyClone/SignInView.swift
@@ -0,0 +1,20 @@
+//
+//  SignInView.swift
+//  SpotifyClone
+//
+//  Created by Gamal Kubeyev on 28.03.2023.
+//
+
+import SwiftUI
+
+struct SignInScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignInScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInScreenView()
+    }
+}
--- SpotifyClone/SpotifyClone/SpotifyCloneApp.swift
+++ SpotifyClone/SpotifyClone/SpotifyCloneApp.swift
@@ -6,12 +6,13 @@
 //
 
 import SwiftUI
+import SpotifyiOS
 
 @main
 struct SpotifyCloneApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            
         }
     }
 }
