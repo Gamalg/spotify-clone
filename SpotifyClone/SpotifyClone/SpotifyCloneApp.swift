8cbc664e59ae9222dfecc04d615e266acd8582ce
Gamal Kubeyev <kubeyev.gamal@gmail.com>
2023-03-28 22:03:27 +0600

    Created project

---
+++ SpotifyClone/SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Gamal Kubeyev on 28.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
