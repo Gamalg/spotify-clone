57e6eed52a8ad4c2a0eb6a0a29bf90c09a065290
Gamal Kubeyev <kubeyev.gamal@gmail.com>
2023-04-24 13:54:30 +0600

    1. Moved to URLRequest func to Request 2. Added prettyPrinted() function to Data object for logging responses

--- SpotifyClone/SpotifyClone/Network/Request.swift
+++ SpotifyClone/SpotifyClone/Network/Request.swift
@@ -20,52 +20,69 @@ protocol Request {
     var method: HTTPMethod { get }
 }
 
-struct Network {
-    enum Errors: Error {
-        case decode
-    }
-    let host: String = "accounts.spotify.com"
-    
-    func request<T: Decodable>(_ request: Request) async throws -> T {
-        let request = urlRequest(from: request)
-        print("Request:", request)
-        do {
-            let data = try await URLSession.shared.data(for: request)
-            print("Response:", String(data: data.0, encoding: .utf8))
-            let jsonDecoder = JSONDecoder()
-            jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
-            let response = try jsonDecoder.decode(T.self, from: data.0)
-            return response
-        } catch {
-            print("Decoding error:", error)
-            throw Errors.decode
-        }
-    }
-    
-    func urlRequest(from request: Request) -> URLRequest {
+extension Request {
+    func toURLRequest(host: String = "accounts.spotify.com") -> URLRequest {
         var urlComponents = URLComponents()
         urlComponents.scheme = "https"
         urlComponents.host = host
-        urlComponents.path = request.path
+        urlComponents.path = path
         
-        let queryItems: [URLQueryItem] = request.parameters.reduce(into: [], {
+        let queryItems: [URLQueryItem] = parameters.reduce(into: [], {
             $0.append(.init(name: $1.key, value: "\($1.value)"))
         })
         
-        if request.method == .GET {
+        if method == .GET {
             urlComponents.queryItems = queryItems
         }
         
         var urlRequest = URLRequest(url: urlComponents.url!)
-        urlRequest.httpMethod = request.method.rawValue
-        urlRequest.allHTTPHeaderFields = request.headers
+        urlRequest.httpMethod = method.rawValue
+        urlRequest.allHTTPHeaderFields = headers
         urlRequest.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        if request.method == .POST {
+        if method == .POST {
             var components = URLComponents()
             components.queryItems = queryItems
             urlRequest.httpBody = components.query?.data(using: .utf8)
         }
         
         return urlRequest
+    }
+}
+
+struct Network {
+    enum Errors: Error {
+        case decode
+    }
+    
+    private let host: String = "accounts.spotify.com"
+    
+    func request<T: Decodable>(_ request: Request) async throws -> T {
+        let request = request.toURLRequest()
+        print("Request:", request)
+        do {
+            let data = try await URLSession.shared.data(for: request)
+            print("Response:", data.0.prettyPrinted())
+            let jsonDecoder = JSONDecoder()
+            jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+            let response = try jsonDecoder.decode(T.self, from: data.0)
+            let jsonEncoder = JSONEncoder()
+            jsonEncoder.outputFormatting = .prettyPrinted
+            return response
+        } catch {
+            print("Decoding error:", error)
+            throw Errors.decode
+        }
+    }
+}
+
+fileprivate extension Data {
+    func prettyPrinted() -> String {
+        do {
+            let jsonObject = try JSONSerialization.jsonObject(with: self, options: .allowFragments)
+            let prettyJsonData = try JSONSerialization.data(withJSONObject: jsonObject, options: .prettyPrinted)
+            return String(data: prettyJsonData, encoding: .utf8) ?? "Failed to pretty print from Data from request"
+        } catch {
+            return "Failed to pretty print from Data from request. Reason \(error.localizedDescription)"
+        }
     }
 }
