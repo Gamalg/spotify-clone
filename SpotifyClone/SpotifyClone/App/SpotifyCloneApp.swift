2de3dd8e066276696cc972901af4c89bd0472e98
Gamal Kubeyev <kubeyev.gamal@gmail.com>
2023-04-24 13:55:08 +0600

    Added check for token and showing corresponding page

--- SpotifyClone/SpotifyClone/App/SpotifyCloneApp.swift
+++ SpotifyClone/SpotifyClone/App/SpotifyCloneApp.swift
@@ -18,19 +18,71 @@ import Foundation
  */
 @main
 struct SpotifyCloneApp: App {
-    private let viewModel = SignInViewModel()
+    @StateObject var viewModel: AppViewModel = AppViewModel()
     var body: some Scene {
         WindowGroup {
-            SignInScreenView()
-//            if viewModel.isSignedIn {
-//                MainPageView()
-//            } else {
-//                SignInScreenView()
-//            }
+            AppCoordinatorView(viewModel: viewModel)
+                .onAppear(perform: viewModel.onAppear)
         }
     }
 }
 
+enum AppState {
+    case signedIn
+    case signedOut
+    case checkingForToken
+}
+
+struct AppCoordinatorView: View {
+    @ObservedObject var viewModel: AppViewModel
+    var body: some View {
+        switch viewModel.state {
+        case .checkingForToken:
+            BlackBGScreen {            
+                ProgressView()
+                    .tint(.white)
+            }
+        case .signedIn:
+            MainPageView()
+        case .signedOut:
+            SignInScreenView()
+        }
+    }
+}
+
+@MainActor
 class AppViewModel: ObservableObject {
-    @Published var isSignedIn: Bool = false
+    @Published var state: AppState = .checkingForToken
+
+    private let tokenStorage = TokenStorage.live
+    private let tokenIdentityClient = TokenIdentityClient()
+    
+    func onAppear() {
+        if let token = tokenStorage.get() {
+            checkForToken(token)
+        } else {
+            state = .signedOut
+        }
+    }
+    
+    private func checkForToken(_ token: Token) {
+        guard token.hasTokenExpired else {
+            self.state = .signedIn
+            return
+        }
+
+        refreshToken(token)
+    }
+    
+    private func refreshToken(_ token: Token) {
+        Task {
+            do {
+                let newToken = try await tokenIdentityClient.refreshToken(refreshToken: token.refreshToken, redirectUri: URL(string: GlobalConstants.redirectURI)!)
+                try tokenStorage.set(newToken)
+                self.state = .signedIn
+            } catch {
+                self.state = .signedOut
+            }
+        }
+    }
 }
